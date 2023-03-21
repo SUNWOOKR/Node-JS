13f1795de6bb30e613a07a8e3bf9de98a5f6fdf5
선우 <ksw-12345@naver.com>
2023-03-21 20:19:52 +0900

    230321

--- 230321/index.js
+++ 230321/index.js
@@ -1,19 +1,22 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require("express")
+const app = express()
+const port = 3000
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  res.send("Hello World!")
+})
 
-app.get("/dog", (req, res) => {
-  res.send("강아지");
-});
+app.get('/user/:id', (req, res) => {
+  const q = req.query
+  console.log({ "name" })
+
+  res.json({'userid': q.name })
+})
 
 app.get("/cat", (req, res) => {
-  res.send("고양이");
-});
+  res.send({ sound: "냐옹" })
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
