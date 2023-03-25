66cf94ea64c97c28c6558e93e9c337e5eaf207b1
선우 <ksw-12345@naver.com>
2023-03-25 11:44:37 +0900

    2023-03-25
    조코딩 마지막

--- 230321/index.js
+++ 230321/index.js
@@ -1,20 +1,35 @@
 const express = require("express")
+var cors = require('cors')
 const app = express()
-const port = 3000
+const port = 8080
+
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
 app.get('/user/:id', (req, res) => {
+  // const q = req.params
+  // console.log(q.id)
   const q = req.query
-  console.log({ "name" })
-
+  console.log(q.q)
+  console.log(q.name)
+  console.log(q.age)
   res.json({'userid': q.name })
 })
 
-app.get("/cat", (req, res) => {
-  res.send({ sound: "냐옹" })
+app.get('/sound/:name', (req, res) => {
+  const { name } = req.params
+  if(name == "dog"){
+    res.json({'sound': '멍멍'})  
+  }else if(name == "cat"){
+    res.json({'sound': '야옹','img':'https://cdn.ccdailynews.com/news/photo/202104/2050720_530952_025.png'})  
+  }else if(name == "pig"){
+    res.json({'sound': '꿀꿀'})  
+  }else {
+    res.json({'sound': '알 수 없음'})
+  }
 })
 
 app.listen(port, () => {
